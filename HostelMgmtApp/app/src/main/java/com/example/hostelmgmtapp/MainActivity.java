104a6886282139a38797027743994fa5d7aab041
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2021-04-09 21:59:29 +0530

    Adding codes

--- HostelMgmtApp/app/src/main/java/com/example/hostelmgmtapp/MainActivity.java
+++ HostelMgmtApp/app/src/main/java/com/example/hostelmgmtapp/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.hostelmgmtapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnlogout,btnFriends,btnMarket;
+    private TextView dashboardName, dashboardRoomno;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore fStore;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnlogout = findViewById(R.id.btnLogout);
+        btnFriends = findViewById(R.id.btnfindMates);
+        btnMarket = findViewById(R.id.btnMarket);
+        dashboardName = findViewById(R.id.nameDashboard);
+        dashboardRoomno = findViewById(R.id.dashboardroomno);
+        fStore = FirebaseFirestore.getInstance();
+        if(FirebaseAuth.getInstance().getCurrentUser() == null){
+            Intent intent = new Intent(MainActivity.this, Registration.class);
+            startActivity(intent);
+            return;
+        }
     }
 }
