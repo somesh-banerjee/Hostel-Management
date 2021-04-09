a7dc0dc8e73df337d72ff9ddc0004574271b733f
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2021-04-09 23:56:12 +0530

    Update Login.java

--- HostelMgmtApp/app/src/main/java/com/example/hostelmgmtapp/Login.java
+++ HostelMgmtApp/app/src/main/java/com/example/hostelmgmtapp/Login.java
@@ -1,5 +1,6 @@
 package com.example.hostelmgmtapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,7 +8,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -31,7 +36,33 @@ public class Login extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                String semail = etlemail.getText().toString();
+                String spassword = etlpassword.getText().toString();
+                if(semail.isEmpty()){
+                    etlemail.setError("Enter Email ID");
+                    etlemail.requestFocus();
+                }
+                else if(spassword.isEmpty()){
+                    etlpassword.setError("Enter Password");
+                    etlpassword.requestFocus();
+                }
+                else if(!(semail.isEmpty() && spassword.isEmpty())){
+                    mFbAuth.signInWithEmailAndPassword(semail,spassword).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()) {
+                                Toast.makeText(Login.this,"Error, try again!",Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Intent i = new Intent(Login.this,MainActivity.class);
+                                startActivity(i);
+                            }
+                        }
+                    });
+                }
+                else {
+                    Toast.makeText(Login.this,"Error Occured!",Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
